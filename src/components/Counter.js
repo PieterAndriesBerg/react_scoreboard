10afe50018e694ec3c6f6367205fe3e9f7c20fbb
Pieter-Andries <p-a.berg@outlook.com>
2020-04-20 11:52:18 +0200

    making Counter.js stateless and only App using State

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Counter extends Component {
-  state = {
-    score: 0,
-  };
+const Counter = (props) => {
+//   incrementScore = () => {
+//     this.setState((prevState) => ({
+//       score: prevState.score + 1,
+//     }));
+//   };
 
-  incrementScore = () => {
-    this.setState((prevState) => ({
-      score: prevState.score + 1,
-    }));
-  };
+//   decrementScore = () => {
+//     this.setState((prevState) => ({
+//       score: prevState.score - 1,
+//     }));
+//   };
 
-  decrementScore = () => {
-    this.setState((prevState) => ({
-      score: prevState.score - 1,
-    }));
-  };
-
-  render() {
     return (
       <div className="counter">
         <button
           className="counter-action decrement"
-          onClick={this.decrementScore}
         >
           {" "}
           -{" "}
         </button>
-        <span className="counter-score">{this.state.score}</span>
+        <span className="counter-score">{props.score}</span>
         <button
           className="counter-action increment"
-          onClick={this.incrementScore}
         >
           {" "}
           +{" "}
         </button>
       </div>
     );
-  }
+
 }
 
 export default Counter;
